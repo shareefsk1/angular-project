cab67065e4fb568c4131d03fddc2b927cd357e9e
shareefsk1 <sharifsk9985@gmail.com>
2022-12-09 12:02:28 +0530

    attribute directives and pipes implemented

--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -11,13 +11,17 @@ export class DirectivesComponent {
 
     public states: string[] = ['telangana','andra','assam','UP','Haryana']
 
-    public users: any = [{name:"shareef",age:20, package:10,Location :"hyd"},
-    {name:"shareef",age:20, package:10,Location :"hyd"},
-    {name:"shareef1",age:21, package:10,Location :"hyd"},
-    {name:"shareef2",age:22, package:10,Location :"hyd"},
-    {name:"shareef3",age:23, package:10,Location :"hyd"},]
+    public users: any = [{name:"shareef",age:9, package:15,Location :"hyd"},
+                          {name:"shareef",age:12, package:10,Location :"hyd"},
+                          {name:"shareef1",age:21, package:11,Location :"hyd"},
+                          {name:"shareef2",age:13, package:9,Location :"hyd"},
+                          {name:"shareef3",age:33, package:15,Location :"hyd"},
+                          {name:"shareef5",age:25, package:5,Location :"hyd"},
+                        ]
 
 
     public time: number = 20;
+
+    public today: any = new Date();
 
 }
