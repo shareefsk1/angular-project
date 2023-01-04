903622eda9a034d75489eccc5bbe5d71b4d778b3
shareefsk1 <sharifsk9985@gmail.com>
2023-01-04 15:12:46 +0530

    custom validators conceot

--- src/app/create-student/create-student.component.ts
+++ src/app/create-student/create-student.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-student',
@@ -12,7 +12,7 @@ export class CreateStudentComponent {
       {                                 
         name: new FormControl(null, [Validators.required, Validators.minLength(3)]),
         phone: new FormControl(null, [Validators.required,Validators.min(100000000), Validators.max(9999999999)]),
-        email: new FormControl(null, [Validators.required, Validators.email ]),
+        email: new FormControl(null, [Validators.required, Validators.email, emailDomain ]),
         // Nested Form
         address: new FormGroup({
           city:new FormControl(null, [Validators.required, Validators.minLength(2)]),
@@ -54,5 +54,18 @@ export class CreateStudentComponent {
     //  Deleteing Form From FormArray
     delete(i:number) {
       this.cardsFormArray.removeAt(i);
+    }
+
+}
+
+// custom validators
+
+function emailDomain(control: AbstractControl): ValidationErrors | null
+{
+    if(control.value?.indexOf("@capgemini.com") != -1){
+      return null ;
+    }
+    else{
+      return {'emailDomain' : 'email should contain @capgemini.com'}
     }
 }
