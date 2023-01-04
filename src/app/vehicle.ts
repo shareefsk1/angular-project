9ccc9f7ec63df959ea09eee8cb2916bfb4734048
shareefsk1 <sharifsk9985@gmail.com>
2023-01-04 15:36:33 +0530

    Custom Datatype Or custom Model Or custom Interface topic

---
+++ src/app/vehicle.ts
@@ -0,0 +1,12 @@
+export interface Vehicle {
+
+    Vehicle: string ;
+    manufacturer: string ;
+    model: string ;
+    color: string;
+    type: string ;
+    fuel: string;
+    image: string;
+    id: string;
+
+}
--- src/app/vehicle/vehicle.component.ts
+++ src/app/vehicle/vehicle.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Vehicle } from '../vehicle';
 import { VehicleService } from '../vehicle.service';
 
 @Component({
@@ -9,7 +10,10 @@ import { VehicleService } from '../vehicle.service';
 })
 export class VehicleComponent {
 
-  public vehicles: any = []
+  
+
+  
+  public vehicles:Vehicle[] = []
 
 
   constructor(private _vehicleService:VehicleService ,  private _router:Router){
